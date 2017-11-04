2ecb3b131d8f4b3137df94e7000bf2eed1540292
Pine Mizune <pinemz@gmail.com>
2017-11-04 14:27:54 +0900

    feat(package.json): add logic put star

---
+++ src/content_scripts.ts
@@ -0,0 +1,19 @@
+'use strict'
+
+function putStar() {
+  const starred = document.querySelector('.starring-container .starred')
+  const unstarred = document.querySelector('.starring-container .unstarred')
+  if (!starred || !unstarred) return
+
+  const starredStyle = document.defaultView.getComputedStyle(starred, '')
+  const isStarred = starredStyle.display !== 'none'
+
+  if (!isStarred) {
+    const starButton = unstarred.querySelector('button')
+    starButton.click()
+  }
+}
+
+putStar()
+
+// vim: se et ts=2 sw=2 sts=2 ft=typescript :
