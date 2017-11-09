32090e32d63127705f3bf7d53394d15ac8256984
Pine Mizune <pinemz@gmail.com>
2017-11-10 03:00:02 +0900

    fix(App.ts): fix delete logic

--- src/options_page/App.ts
+++ src/options_page/App.ts
@@ -53,7 +53,12 @@ export default class AppComponent extends Vue {
     })
   }
 
-  handleDelete(index: number) {
+  async handleDelete(index: number) {
     this.patterns.splice(index, 1)
+
+    await saveConfigForBackground(this.config)
+    if (DEBUG) {
+      console.log('saved', this.config)
+    }
   }
 }
